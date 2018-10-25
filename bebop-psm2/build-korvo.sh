13bcafb87789820341c602cf31fcb36a637cbadf
tongshu83 <ts372@njit.edu>
2018-10-25 15:39:23 -0500

    Update build-korvo.sh

--- bebop-psm2/build-korvo.sh
+++ bebop-psm2/build-korvo.sh
@@ -16,7 +16,7 @@ echo Loading modules...
 module unload intel-mkl/2017.3.196-v7uuj6z
 module load gcc/7.1.0
 module load libpsm2/10.3-17
-module load cmake/3.12.2-4zllpyo
+module load cmake
 echo Modules OK
 
 set -eu
