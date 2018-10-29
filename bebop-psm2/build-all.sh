c49380d6f44a5a596513350808647a2e3496ac0e
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-10-29 14:13:10 -0500

    Create ROOT parents; capture BUILD_LOG

--- bebop-psm2/build-all.sh
+++ bebop-psm2/build-all.sh
@@ -1,16 +1,25 @@
 #!/bin/bash -l
 
 set -a
-export ROOT=$PWD/install
+ROOT=$HOME/Public/sfw/bebop/codar-install
 if [ ! -d $ROOT ]
 then
-	mkdir $ROOT
+  mkdir -pv $ROOT
 fi
 
+BUILD_LOG=build-$( date "+%Y-%m-%d-%H:%M_%p" ).log
+
+{
+echo ROOT=$ROOT
+echo
+source modules.sh
 source build-korvo.sh
+# source env_korvo.sh
 source build-adios.sh
+# source env_adios.sh
 source build-ht_sw.sh
-source build-swiftT.sh 
+source build-swiftT.sh
+# source env_swiftT.sh
 source build-lammps.sh
 source build-mpi_in_mpi.sh
-
+} 2>& 1 | tee $BUILD_LOG
