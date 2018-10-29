a1535a65a78c4bff3459c798908f5a098a492ef1
tongshu83 <ts372@njit.edu>
2018-10-29 18:41:26 -0500

    remove wf-launch_multi/*.tic

---
+++ bebop-psm2/workflows/wf-launch_envs/workflow-sh.swift
@@ -0,0 +1,40 @@
+import io;
+import launch;
+import string;
+import sys;
+
+(void v) setup_run(string dir) "turbine" "0.0"
+[
+	"""
+	file mkdir <<dir>>
+	"""
+];
+
+main()
+{
+	// Process counts
+	int proc = 2;
+
+	// Command
+	string cmd;
+	cmd = "../../../../scripts/script1.sh";
+
+	// Command line arguments
+	string args[] = [""];
+
+	// Environment variables
+	string turbine_output = getenv("TURBINE_OUTPUT");
+	string outdir = "%s/run" % turbine_output;
+	string envs[] = [ "swift_chdir="+outdir ];
+	// string envs[] = [ "swift_chdir="+outdir, "swift_output="+outdir/"output_script1.txt" ];
+
+	printf("swift: launching with environment variables: %s", cmd);
+	setup_run(outdir) =>
+		exit_code = @par=proc launch_envs(cmd, args, envs);
+	printf("swift: received exit code: %d", exit_code);
+	if (exit_code != 0)
+	{
+		printf("swift: The launched application did not succeed.");
+	}
+}
+
---
+++ bebop-psm2/workflows/wf-launch_envs/workflow.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -eu
+
+# WORKFLOW SH
+# Main user entry point
+
+if [[ ${#} != 1 ]]
+then
+	echo "Usage: ./workflow.sh workflow_name"
+	exit 1
+fi
+
+WORKFLOW_SWIFT=$1.swift
+WORKFLOW_TIC=${WORKFLOW_SWIFT%.swift}.tic
+
+# Turn off Swift/T debugging
+export TURBINE_LOG=0 TURBINE_DEBUG=0 ADLB_DEBUG=0
+
+# Find the directory of ./workflow.sh
+export WORKFLOW_ROOT=$( cd $( dirname $0 ) ; /bin/pwd )
+cd $WORKFLOW_ROOT
+
+# Set the output directory
+export TURBINE_OUTPUT=$WORKFLOW_ROOT/experiment
+mkdir -pv $TURBINE_OUTPUT
+
+# Total number of processes available to Swift/T
+# Of these, 2 are reserved for the system
+export PROCS=36
+export PPN=36
+export WALLTIME=00:01:00
+
+MACHINE="" #"-m slurm" # -m (machine) option that accepts pbs, cobalt, cray, lsf, theta, or slurm
+
+ENVS="" # "-e <key>=<value>" Set an environment variable in the job environment.
+
+set -x
+stc -p $WORKFLOW_ROOT/$WORKFLOW_SWIFT
+# -p: Disable the C preprocessor
+
+turbine -l $MACHINE -n $PROCS $ENVS $WORKFLOW_ROOT/$WORKFLOW_TIC
+# -l: Enable mpiexec -l ranked output formatting
+# -n <procs>: The total number of Turbine MPI processes
+
+#swift-t -l $MACHINE -p -n $PROCS $ENVS $WORKFLOW_ROOT/workflow.swift
+
+echo WORKFLOW COMPLETE.
+
