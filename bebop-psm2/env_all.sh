fe651c3c87ecfa85983789e22fbd608cb4e990e6
tongshu83 <ts372@njit.edu>
2018-10-29 17:29:42 -0500

    Update env_all.sh

--- bebop-psm2/env_all.sh
+++ bebop-psm2/env_all.sh
@@ -4,9 +4,10 @@ set -a
 
 echo Loading modules...
 module unload intel-mkl/2017.3.196-v7uuj6z
-module load gcc/7.1.0
-module load libpsm2/10.3-17
+# module load gcc/7.1.0
 module load intel/17.0.4-74uvhji
+module load intel-mpi/2017.3-dfphq6k
+module load libpsm2/10.3-17
 module load cmake/3.12.2-4zllpyo
 module load jdk/8u141-b15-mopj6qr
 module load tcl/8.6.6-x4wnbsg
