5c53c4c1352c027d8e2391e72b98acf8ec32e425
tongshu83 <ts372@njit.edu>
2018-10-31 12:20:09 -0500

    update

--- bebop-psm2/sbatch-bebop.sh
+++ bebop-psm2/sbatch-bebop.sh
@@ -13,7 +13,8 @@ export I_MPI_FABRICS=shm:tmi
 # /usr/bin/time -v -o time_c-mpi.txt /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/c-mpi.x 2 38 bdw-0552 37 bdw-0553
 # /usr/bin/time -v -o time_c-mpi.txt /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/c-mpi.x 2 38 bdw-0552,bdw-0553 34 bdw-0552,bdw-0553
 # /usr/bin/time -v -o time_mpi-sh.txt mpiexec -n 72 -ppn 72 /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/mpi-sh.x 
-/usr/bin/time -v -o time_mpi-mpi.txt mpiexec -n 2 -ppn 2 /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/mpi-mpi.x 38 bdw-0552,bdw-0553 34 bdw-0552,bdw-0553
-# /usr/bin/time -v -o time_mpi-ht.txt mpiexec -n 2 -ppn /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/mpi-ht.x 12 bdw-0093,bdw-0094 3 bdw-0093,bdw-0094
-# /usr/bin/time -v -o time_mpi-lammps.txt mpiexec -n 2 -ppn 2 /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/mpi-lammps.x 24 bdw-0093,bdw-0094 14 bdw-0093,bdw-0094
+# /usr/bin/time -v -o time_mpi-mpi.txt mpiexec -n 2 -ppn 2 /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/mpi-mpi.x 38 bdw-0552,bdw-0553 34 bdw-0552,bdw-0553
+# /usr/bin/time -v -o time_mpi-ht.txt mpiexec -n 2 -ppn 2 /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/mpi-ht.x 36 bdw-0098 36 bdw-0099
+# /usr/bin/time -v -o time_mpi-ht.txt mpiexec -n 2 -ppn 2 /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/mpi-ht.x 40 bdw-0098,bdw-0099 14 bdw-0098,bdw-0099
+# /usr/bin/time -v -o time_mpi-lammps.txt mpiexec -n 2 -ppn 2 /blues/gpfs/home/tshu/project/bebop/MPI_in_MPI/bebop-psm2/mpi-lammps.x 36 bdw-0098 36 bdw-0099
 
