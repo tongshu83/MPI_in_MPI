b00c0aed381faab1463a1b64686ac8f831210a98
tongshu83 <ts372@njit.edu>
2019-01-11 00:13:31 -0600

    add ppw

--- bebop-psm2/workflows/wf-launch_env_lp/workflow-lmp.swift
+++ bebop-psm2/workflows/wf-launch_env_lp/workflow-lmp.swift
@@ -10,7 +10,7 @@ import sys;
 	file delete -force -- <<dir>>
 	file mkdir <<dir>>
 	cd <<dir>>
-	file link -symbolic in.quench.short <<infile1>>
+	file copy -force -- <<infile1>> in.quench.short
 	file link -symbolic restart.liquid <<infile2>>
 	file link -symbolic CuZr.fs <<infile3>>
 """
@@ -26,34 +26,43 @@ import sys;
 (int exit_code) launch(string run_id, int params[])
 {
 	string workflow_root = getenv("WORKFLOW_ROOT");
+	string turbine_output = getenv("TURBINE_OUTPUT");
+	string dir = "%s/run/%s" % (turbine_output, run_id);
+	string infile1 = "%s/in.quench.short" % turbine_output;
+	string infile2 = "%s/restart.liquid" % turbine_output;
+	string infile3 = "%s/CuZr.fs" % turbine_output;
+	int ppw = 35;
 
-	string cmd0[] = [ workflow_root/"lmp.sh", int2string(params[4]), "POSIX" ];
-	(output0, exit_code0) = system(cmd0);
+	string cmd0[] = [ workflow_root/"lmp.sh", int2string(params[2]), "POSIX", dir/"in.quench.short" ];
+	setup_input(dir, infile1, infile2, infile3) =>
+		(output0, exit_code0) = system(cmd0);
+
 	if (exit_code0 != 0)
 	{
-		printf("swift: %s failed with exit code %d.", cmd0[0]+" "+cmd0[1]+" "+cmd0[2], exit_code0);
+		printf("swift: %s failed with exit code %d.", cmd0[0]+" "+cmd0[1]+" "+cmd0[2]+" "+cmd0[3], exit_code0);
 		exit_code = exit_code0;
 	}
 	else
 	{
-		string turbine_output = getenv("TURBINE_OUTPUT");
-		string dir = "%s/run/%s" % (turbine_output, run_id);
-		string infile1 = "%s/in.quench.short" % turbine_output;
-		string infile2 = "%s/restart.liquid" % turbine_output;
-		string infile3 = "%s/CuZr.fs" % turbine_output;
-
-		int proc1 = params[0];
+		int nwork1;
+		if (params[0] %% ppw == 0) {
+			nwork1 = params[0] %/ ppw;
+		} else {
+			nwork1 = params[0] %/ ppw + 1;
+		}
 		string cmd1 = "../../../../../../Example-LAMMPS/swift-all/lmp_mpi"; 
 		string args1[] = split("-i in.quench.short", " ");	// mpiexec -n 8 ./lmp_mpi -i in.quench.short
 		string envs1[] = [ "OMP_NUM_THREADS="+int2string(params[1]), 
 		       "swift_chdir="+dir, 
 		       "swift_output="+dir/"output_lmp_mpi.txt", 
-		       "swift_exectime="+dir/"time_lmp_mpi.txt" ];
+		       "swift_exectime="+dir/"time_lmp_mpi.txt",
+		       "swift_numproc=%i" % params[0],
+		       "swift_ppw=%i" % ppw ];
 
 		printf("swift: launching with environment variables: %s", cmd1);
 		sleep(1) =>
-			setup_input(dir, infile1, infile2, infile3) =>
-			exit_code1 = @par=proc1 launch_envs(cmd1, args1, envs1);
+			exit_code1 = @par=nwork1 launch_envs(cmd1, args1, envs1);
+
 		if (exit_code1 != 0)
 		{
 			printf("swift: %s failed with exit code %d.", cmd1, exit_code1);
@@ -65,6 +74,7 @@ import sys;
 			sleep(1) =>
 				setup_run(dir) => 
 				(output2, exit_code2) = system(cmd2);
+
 			if (exit_code2 != 0)
 			{
 				printf("swift: %s failed with exit code %d.", "bpmeta --nthreads 8 dump.bp", exit_code2);
@@ -72,18 +82,25 @@ import sys;
 			}
 			else
 			{
-				int proc3 = params[2];
+				int nwork3;
+				if (params[3] %% ppw == 0) {
+					nwork3 = params[3] %/ ppw;
+				} else {
+					nwork3 = params[3] %/ ppw + 1;
+				}
 				string cmd3 = "../../../../../../Example-LAMMPS/swift-all/voro_adios_omp_staging";
 				string args3[] = split("dump.bp adios_atom_voro.bp BP", " ");     // mpiexec -n 4 ./voro_adios_omp_staging dump.bp adios_atom_voro.bp BP
-				string envs3[] = [ "OMP_NUM_THREADS="+int2string(params[3]), 
+				string envs3[] = [ "OMP_NUM_THREADS="+int2string(params[4]), 
 				       "swift_chdir="+dir, 
 				       "swift_output="+dir/"output_voro_adios_omp_staging.txt", 
-				       "swift_exectime="+dir/"time_voro_adios_omp_staging.txt" ];
+				       "swift_exectime="+dir/"time_voro_adios_omp_staging.txt", 
+				       "swift_numproc=%i" % params[3], 
+				       "swift_ppw=%i" % ppw ];
 
 				printf("swift: launching with environment variables: %s", cmd3);
 				sleep(1) =>
-					setup_run(dir) =>
-					exit_code3 = @par=proc3 launch_envs(cmd3, args3, envs3);
+					exit_code3 = @par=nwork3 launch_envs(cmd3, args3, envs3);
+
 				if (exit_code3 != 0)
 				{
 					printf("swift: %s failed with exit code %d.", cmd3, exit_code3);
@@ -102,9 +119,9 @@ main()
 {
 	int codes[];
 
-	int params_start[] = [2, 2, 2, 2, 100];
-	int params_stop[] = [3, 3, 3, 3, 200];
-	int params_step[] = [1, 1, 1, 1, 100];
+	int params_start[] = [35, 2, 100, 35, 2];
+	int params_stop[] = [70, 3, 200, 70, 3];
+	int params_step[] = [35, 1, 100, 35, 1];
 	int params_num[] = [ (params_stop[0] - params_start[0]) %/ params_step[0] + 1,
 	    (params_stop[1] - params_start[1]) %/ params_step[1] + 1,
 	    (params_stop[2] - params_start[2]) %/ params_step[2] + 1,
@@ -126,7 +143,7 @@ main()
 							+ (param2 - params_start[2]) %/ params_step[2] * params_num[3] * params_num[4]
 							+ (param3 - params_start[3]) %/ params_step[3] * params_num[4]
 							+ (param4 - params_start[4]) %/ params_step[4];
-						codes[i] = launch("%0.2i_%0.2i_%0.2i_%0.2i_%0.2i" % (param0, param1, param2, param3, param4), [param0, param1, param2, param3, param4]);
+						codes[i] = launch("%0.1i_%0.1i_%0.3i_%0.1i_%0.1i" % (param0, param1, param2, param3, param4), [param0, param1, param2, param3, param4]);
 						if (codes[i] != 0)
 						{
 							printf("swift: The launched applications with parameters (%d, %d, %d, %d, %d) did not succeed with exit code: %d.",
